b785a7bb9ac00e12aa65e4ded8e780dbdaf7d9dd
David Frank <davidfrank96.df@gmail.com>
2019-02-12 08:26:52 -0800

    err message

--- server/src/controllers/menuControllers.js
+++ server/src/controllers/menuControllers.js
@@ -3,13 +3,10 @@ import menu from "../db/menu";
 class MenuControllers {
   //Get Menu for the Single day
   static getMenu(req, res) {
-      return res.status(200).json({
-          status: res.statusCode,
-          data: menu
-      });
-      return res.status(404).json({
-          message: "Not Found"
-      });
+    return res.status(200).json({
+      status: res.statusCode,
+      data: menu
+    });
   }
 }
 
